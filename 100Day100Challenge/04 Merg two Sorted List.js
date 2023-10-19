0869aab66ed574ebc1e4d09baa5994237d3d97e1
Hiro <d3vaqa@gmail.com>
2023-10-19 21:57:16 +1100

    fixed typo and added the ListNOde function

--- 100Day100Challenge/04 Merg two Sorted List.js
+++ 100Day100Challenge/04 Merg two Sorted List.js
@@ -1,7 +1,7 @@
 //Challenge: Merge two sorted linked lists and return it as a new sorted list.
 
 const mergeTwoLists = (l1, l2) =>{
-    let preHead = new listNode (-1)
+    let preHead = new ListNode (-1)
     let prev = preHead
 
     while(l1 !== null && l2 !== null){
@@ -22,3 +22,28 @@ const mergeTwoLists = (l1, l2) =>{
 
     return preHead.next
 }
+
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+let l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+let l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+const mergedList = mergeTwoLists(l1, l2);
+
+
+console.log(mergedList)
+
+function printList(node) {
+    while (node) {
+        console.log(node.val);
+        node = node.next;
+    }
+}
+
+
+printList(mergedList);
+
